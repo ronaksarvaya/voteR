e1f288504962a8e5021eed74cbf27159eba89929
ronaksarvaya <ronaksarvaiya2@gmail.com>
2025-04-12 23:05:32 +0530

    ... commit message

--- frontend/src/User.jsx
+++ frontend/src/User.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { API_URL } from "/config";
+import { API_URL } from "./config";
 
 const User = () => {
   const [fullname, setFullname] = useState("");
